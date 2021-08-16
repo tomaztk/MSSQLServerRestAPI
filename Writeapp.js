d9b639e87699870348121194cab2b38948d7e77a
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-08-16 21:13:51 +0200

    Update Writeapp.js

--- Writeapp.js
+++ Writeapp.js
@@ -3,9 +3,6 @@ const express = require('express');
 const app = express();
 const sql = require('mssql/msnodesqlv8') //mssql with MS driver for SQL Server
 var beautify = require("json-beautify");
-// npm install url-parse
-//var Url = require('url-parse');
- 
  
 var env = process.env.NODE_ENV || 'production';
 var sqlConfig = require('./config')[env];
@@ -32,26 +29,17 @@ const connection = new sql.ConnectionPool(sqlConfig, function(err){
       }
     }
 )
-//const URL = require("url").URL;
 
+
+// for test purposes
 app.get('/insert', function(req,res){
 	
-	//var url_parts = url.parse(req.url, true);
-	//var query = url_parts.query;
 	var val = req.query.val;
 	var val2 = req.query.val2;
-
-    //var val = req.query.name
       
-    console.log("val :", val)
-        console.log("val2 :", val2)
+  console.log("val :", val)
+  console.log("val2 :", val2)
     
-	
-//console.log(val);
-//console.log(url_parts);
-//console.log(query);
-
-
 connection.connect(function(err) {
 connection.query("insert into usersad2 (val,val2) values ("+val+","+val2+")",function(err,result){
 
@@ -64,7 +52,59 @@ connection.query("SELECT * FROM usersAD2", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
   });
+  res.send('Successfully inserted in MSSQLServer Database!');
+}});});
+
+
+});
+
+
+
+app.post('/insert/UsersAD', function(req,res){
+	
+	var EmloyeeID = req.query.EmloyeeID;
+	var SamAccountName = req.query.SamAccountName;
+  var DisplayName = req.query.DisplayName;
+  var Email = req.query.Email;
+
+  console.log(EmloyeeID);
+  console.log(SamAccount);
+
+    
+connection.connect(function(err) {
+connection.query("INSERT INTO dbo.UsersAD (EmloyeeID, SamAccountName,DisplayName, Email) values ("+EmloyeeID+",'"+SamAccountName+"', '"+DisplayName+"', '"+Email+"')",function(err,result){
+
+if(!!err){
+console.log(err);
+res.send('Error in inserting');
+}
+else{
   res.send('Successfully Insertion');
+}});});
+
+});
+
+
+app.get('/inset', function(req,res){
+	
+	var t = req.query.t.toString();
+  //var tt = t.toString(); 
+  
+    
+connection.connect(function(err) {
+  connection.query(" insert into tt (t) values (" + t + ")",function(err,result){
+
+
+if(!!err){
+console.log(err);
+res.send('Error in inserting');
+}
+else{
+connection.query("SELECT * FROM tt", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+  res.send('Successfully inserted in MSSQLServer Database!');
 }});});
 
 
