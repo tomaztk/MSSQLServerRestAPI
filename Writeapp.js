4db1dfb14e67272e1a022eef73c79c28084a73c3
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-08-16 22:16:45 +0200

    update

--- Writeapp.js
+++ Writeapp.js
@@ -31,76 +31,77 @@ const connection = new sql.ConnectionPool(sqlConfig, function(err){
 )
 
 
-// for test purposes
+// // for test purposes
+// app.get('/insert', function(req,res){
+	
+// 	var val = req.query.val;
+// 	var val2 = req.query.val2;
+      
+//   console.log("val :", val)
+//   console.log("val2 :", val2)
+    
+// connection.connect(function(err) {
+// connection.query("insert into usersad2 (val,val2) values ("+val+","+val2+")",function(err,result){
+
+// if(!!err){
+// console.log(err);
+// res.send('Error in inserting');
+// }
+// else{
+// connection.query("SELECT * FROM usersAD2", function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+//   res.send('Successfully inserted in MSSQLServer Database!');
+// }});});
+
+
+// });
+
+
+
+// app.post('/insert/UsersAD', function(req,res){
+	
+// 	var EmloyeeID = req.query.EmloyeeID;
+// 	var SamAccountName = req.query.SamAccountName;
+//   var DisplayName = req.query.DisplayName;
+//   var Email = req.query.Email;
+
+//   console.log(EmloyeeID);
+//   console.log(SamAccount);
+
+    
+// connection.connect(function(err) {
+// connection.query("INSERT INTO dbo.UsersAD (EmloyeeID, SamAccountName,DisplayName, Email) values ("+EmloyeeID+",'"+SamAccountName+"', '"+DisplayName+"', '"+Email+"')",function(err,result){
+
+// if(!!err){
+// console.log(err);
+// res.send('Error in inserting');
+// }
+// else{
+//   res.send('Successfully Insertion');
+// }});});
+
+// });
+
+
+
 app.get('/insert', function(req,res){
 	
-	var val = req.query.val;
-	var val2 = req.query.val2;
-      
-  console.log("val :", val)
-  console.log("val2 :", val2)
-    
-connection.connect(function(err) {
-connection.query("insert into usersad2 (val,val2) values ("+val+","+val2+")",function(err,result){
-
-if(!!err){
-console.log(err);
-res.send('Error in inserting');
-}
-else{
-connection.query("SELECT * FROM usersAD2", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-  res.send('Successfully inserted in MSSQLServer Database!');
-}});});
+	var tekst = req.params.tekst;    
+  console.log("value for t :", tekst.toString());
 
 
-});
-
-
-
-app.post('/insert/UsersAD', function(req,res){
-	
-	var EmloyeeID = req.query.EmloyeeID;
-	var SamAccountName = req.query.SamAccountName;
-  var DisplayName = req.query.DisplayName;
-  var Email = req.query.Email;
-
-  console.log(EmloyeeID);
-  console.log(SamAccount);
-
-    
-connection.connect(function(err) {
-connection.query("INSERT INTO dbo.UsersAD (EmloyeeID, SamAccountName,DisplayName, Email) values ("+EmloyeeID+",'"+SamAccountName+"', '"+DisplayName+"', '"+Email+"')",function(err,result){
-
-if(!!err){
-console.log(err);
-res.send('Error in inserting');
-}
-else{
-  res.send('Successfully Insertion');
-}});});
-
-});
-
-
-app.get('/inset', function(req,res){
-	
-	var t = req.query.t.toString();
-  //var tt = t.toString(); 
   
-    
 connection.connect(function(err) {
-  connection.query(" insert into tt (t) values (" + t + ")",function(err,result){
-
+connection.query("INSERT  INTO TestChar (tekst) values (''"+String(tekst)+"'')",function(err,result){
 
 if(!!err){
 console.log(err);
 res.send('Error in inserting');
 }
 else{
-connection.query("SELECT * FROM tt", function (err, result, fields) {
+connection.query("SELECT * FROM TestChar", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
   });
@@ -109,3 +110,6 @@ connection.query("SELECT * FROM tt", function (err, result, fields) {
 
 
 });
+
+
+
